ec99c21735449dd6c8e6e6047fa46d526c212cf5
Johanna Merikanto <johanna.merikanto@outlook.com>
2020-05-14 11:27:00 +0300

    Projekti liitetty GitHubiin 14.5.2020.

---
+++ Controllers/TilausController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TilausDBMVC.Models;
+
+namespace TilausDBMVC.Controllers
+{
+    public class TilausController : Controller
+    {
+        private TilausDBEntities db = new TilausDBEntities();
+
+        // GET: Tilaus
+        public ActionResult Index()
+        {
+            var tilaukset = db.Tilaukset.Include(t => t.Asiakkaat).Include(t => t.Postitoimipaikat);
+            return View(tilaukset.ToList());
+        }
+
+        // GET: Tilaus/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tilaukset tilaukset = db.Tilaukset.Find(id);
+            if (tilaukset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tilaukset);
+        }
+
+        // GET: Tilaus/Create
+        public ActionResult Create()
+        {
+            ViewBag.AsiakasID = new SelectList(db.Asiakkaat, "AsiakasID", "Nimi");
+            ViewBag.Postinumero = new SelectList(db.Postitoimipaikat, "Postinumero", "Postitoimipaikka");
+            return View();
+        }
+
+        // POST: Tilaus/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TilausID,AsiakasID,Toimitusosoite,Postinumero,Tilauspvm,Toimituspvm")] Tilaukset tilaukset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tilaukset.Add(tilaukset);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AsiakasID = new SelectList(db.Asiakkaat, "AsiakasID", "Nimi", tilaukset.AsiakasID);
+            ViewBag.Postinumero = new SelectList(db.Postitoimipaikat, "Postinumero", "Postitoimipaikka", tilaukset.Postinumero);
+            return View(tilaukset);
+        }
+
+        // GET: Tilaus/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tilaukset tilaukset = db.Tilaukset.Find(id);
+            if (tilaukset == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AsiakasID = new SelectList(db.Asiakkaat, "AsiakasID", "Nimi", tilaukset.AsiakasID);
+            ViewBag.Postinumero = new SelectList(db.Postitoimipaikat, "Postinumero", "Postitoimipaikka", tilaukset.Postinumero);
+            return View(tilaukset);
+        }
+
+        // POST: Tilaus/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TilausID,AsiakasID,Toimitusosoite,Postinumero,Tilauspvm,Toimituspvm")] Tilaukset tilaukset)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tilaukset).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AsiakasID = new SelectList(db.Asiakkaat, "AsiakasID", "Nimi", tilaukset.AsiakasID);
+            ViewBag.Postinumero = new SelectList(db.Postitoimipaikat, "Postinumero", "Postitoimipaikka", tilaukset.Postinumero);
+            return View(tilaukset);
+        }
+
+        // GET: Tilaus/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tilaukset tilaukset = db.Tilaukset.Find(id);
+            if (tilaukset == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tilaukset);
+        }
+
+        // POST: Tilaus/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tilaukset tilaukset = db.Tilaukset.Find(id);
+            db.Tilaukset.Remove(tilaukset);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
